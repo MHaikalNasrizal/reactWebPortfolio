22482888ddf9f56def3f073694b75de123de4997
MHaikalNasrizal <haikalnasrizal321@gmail.com>
2023-04-21 16:57:38 +0800

    Navbar

---
+++ src/componets/Navbar.tsx
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function Navbar() {
+  return (
+    <>
+      <div className="navbar bg-body-tertiary">
+        <div className="container-fluid">
+          <div className="navbar-brand">hello</div>
+        </div>
+      </div>
+    </>
+  );
+}
