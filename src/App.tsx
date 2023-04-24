008c1b086b2053c5efa3fbb53acd8e6db358bb54
MHaikalNasrizal <haikalnasrizal321@gmail.com>
2023-04-24 15:44:18 +0800

    Cover page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Navbar from './componets/Navbar'
+import Home from './componets/Home'
 
 function App() {
   
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <Navbar />
+      <Home />
       
     </>
   )
